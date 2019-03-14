3b4b4847f7a18a45ac35b24e3c339e59ea3caf1f
Jack Lynch <jacklynchtds@gmail.com>
2019-03-13 22:37:25 -0400

    Invite members from membership CSV

--- invite_members.py
+++ invite_members.py
@@ -1,18 +1,18 @@
-from github import Github
-from github import GithubException
+from github import Github, GithubException
 import sys
+import pandas as pd
+import time
 # https://pygithub.readthedocs.io/en/latest/introduction.html
 
 uname = sys.argv[1]
 pw = sys.argv[2]
-member_emails = ['jacklynchtds@gmail.com']
+
+csv_path = 'members.csv'
+membership = pd.read_csv(csv_path)
+member_emails = membership.iloc[:,0].values.tolist()
 
 g = Github(uname, pw)
 aic = Github.get_organization(g, 'aiatncsu')
-
-print("getting members")
-#print(aic)
-#print(aic.get_members()[1])
 
 members = []
 
@@ -20,18 +20,26 @@ for email in member_emails:
     query = email + ' in:email'
     unames = g.search_users(query)
     uname = ''
-    if unames.totalCount == 0:
-        continue
-    else:
-        uname = unames[0]
+    try:
+        if unames.totalCount == 0:
+            continue
+        else:
+            uname = unames[0]
+    except GithubException as e:
+        if e[0] == 403:  # sent too many requests in the time limit
+            sys.exit()
 
     members.append(uname)
 
-#print(members)
+print(members)
 
 for member in members:
     try:
         aic.invite_user(member)
     except GithubException as e:
+        # verbose way of checking the proper exception
         if e[1]['errors'][0]['message'] == 'Invitee is already a part of this org':
             continue
+        # less-verbose way (check the error code)
+        if e[0] == 403:  # sent too many requests in the time limit
+            sys.exit()
